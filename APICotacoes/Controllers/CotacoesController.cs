08e77a3229a05b8925b7e8471182ce791cb7f904
Renato Groffe <renato.groffe@yahoo.com.br>
2019-11-30 03:51:11 -0300

    ASP.NET Core 3.0 + API REST + Dapper + SQL Server + Azure Key Vault
    ASP.NET Core 3.0 + API REST + Dapper + SQL Server + Azure Key Vault

---
+++ APICotacoes/Controllers/CotacoesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace APICotacoes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CotacoesController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Get(
+            [FromServices]IConfiguration config)
+        {
+            using var conexao = new SqlConnection(
+                config.GetConnectionString("BaseCotacoes"));
+
+            string valorJSON = conexao.QueryFirst<string>(
+                "SELECT Sigla " +
+                      ",NomeMoeda " +
+                      ",UltimaCotacao " +
+                      ",ValorComercial AS 'Cotacoes.FCNuvem.Comercial' " +
+                      ",ValorTurismo AS 'Cotacoes.FCNuvem.Turismo' " +
+                "FROM dbo.Cotacoes " +
+                "ORDER BY NomeMoeda " +
+                "FOR JSON PATH, ROOT('Moedas')");
+            
+            return Content(valorJSON, "application/json");
+        }
+    }
+}
